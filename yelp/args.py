ea3979939b451cff5918abd77cb7f0aae5145730
Sudarshan <su0@ornl.gov>
2019-03-23 15:26:28 -0400

    Added more args and full preproc dataset

--- yelp/args.py
+++ yelp/args.py
@@ -3,15 +3,20 @@
 from argparse import Namespace
 
 args = Namespace(
+    raw_train_csv='raw_train.csv',
+    raw_test_csv='raw_test.csv',
+    train_proportion=0.7,
+    full_file='reviews_with_splits_full.csv',
     frequency_cutoff=25, # minimum frequency for words to be counted
-    workdir_name='scratch',
+    lite_dir='models/lite',
+    full_dir='models/full',
     vectorizer_fname='vectorizer.json',
     batch_size=1024,
     early_stopping_criteria=5,
     learning_rate=0.001,
     num_epochs=100,
     device='cuda:3',
-    sample_file='reviews_with_splits_lite.csv',
+    lite_file='reviews_with_splits_lite.csv',
     checkpointer_prefix='yelp',
     checkpointer_name='classifier',
     save_every=2, # save model every n epochs
