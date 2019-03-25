105b0215597b8309d473d857039d0c0d8be9e436
Sudarshan <su0@ornl.gov>
2019-03-25 19:40:45 -0400

    OOPd dataset

--- surname/surname/vectorizer.py
+++ surname/surname/vectorizer.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
+import pdb
 import pandas as pd
 import numpy as np
 import string
 
 from .vocab import Vocabulary
 
+# class Vectorizer(object):
+  # """
+    # The Vectorizer which coordinates the vocabularies and puts them to use
+  # """
+  # def __init__(self, surname_vocab: Vocabulary, nationality_vocab: Vocabulary) -> None:
+    # """
+      # Args:
+        # surname_vocab    : maps characters to integers
+        # nationality_vocab: maps nationalities to integers
+    # """
+    # self.surname_vocab = surname_vocab
+    # self.nationality_vocab = nationality_vocab
+
+
 class CNNVectorizer(object):
-  """
-    The Vectorizer which coordinates the vocabularies and puts them to use
-  """
-  def __init__(self, surname_vocab: Vocabulary, nationality_vocab: Vocabulary, max_surname_length:
-      int) -> None:
-    """
-      Args:
-        surname_vocab     : maps characters to integers
-        nationality_vocab : maps nationalities to integers
-        max_surname_length: the length of the longest surname
-    """
+  def __init__(self, surname_vocab, nationality_vocab, max_surname_length):
     self.surname_vocab = surname_vocab
     self.nationality_vocab = nationality_vocab
     self.max_surname_length = max_surname_length
-
 
   def vectorize(self, surname: str) -> np.ndarray:
     """
@@ -33,14 +37,23 @@ class CNNVectorizer(object):
       Returns:
         one_hot_matrix: a matrix of one-hot vectors
     """
-    one_hot = np.zeros((len(self.surname_vocab), self.max_surname_length), dtype=np.float32)
+    one_hot_matrix = np.zeros((len(self.surname_vocab), self.max_surname_length), dtype=np.float32)
 
     for idx, char in enumerate(surname):
       char_idx = self.surname_vocab.lookup_token(char)
-      one_hot[char_idx][idx] = 1
+      one_hot_matrix[char_idx][idx] = 1
 
     return one_hot_matrix
 
+  @classmethod
+  def from_serializable(cls, contents: dict):
+    """
+      Instantiates a Vectorizer from a serializable dictionary
+    """
+    surname_vocab = Vocabulary.from_serializable(contents['surname_vocab'])
+    nationality_vocab = Vocabulary.from_serializable(contents['nationality_vocab'])
+    max_surname_length = Vocabulary.from_serializable(contents['max_surname_length'])
+    return cls(surname_vocab, nationality_vocab, max_surname_length)
 
   @classmethod
   def from_dataframe(cls, surname_df: pd.DataFrame):
@@ -66,18 +79,6 @@ class CNNVectorizer(object):
 
     return cls(surname_vocab, nationality_vocab, max_surname_length)
 
-  @classmethod
-  def from_serializable(cls, contents: dict):
-    """
-      Instantiates a Vectorizer from a serializable dictionary
-    """
-    surname_vocab = Vocabulary.from_serializable(contents['surname_vocab'])
-    nationality_vocab = Vocabulary.from_serializable(contents['nationality_vocab'])
-    max_surname_length = contents['max_surname_length']
-
-    return cls(surname_vocab=surname_vocab, nationality_vocab=nationality_vocab,
-        max_surname_length=max_surname_length)
-
   def to_serializeable(self) -> dict:
     """
       Create the serializable dictionary for caching
@@ -89,18 +90,9 @@ class CNNVectorizer(object):
         }
 
 class MLPVectorizer(object):
-  """
-    The Vectorizer which coordinates the vocabularies and puts them to use
-  """
   def __init__(self, surname_vocab: Vocabulary, nationality_vocab: Vocabulary) -> None:
-    """
-      Args:
-        surname_vocab    : maps characters to integers
-        nationality_vocab: maps nationalities to integers
-    """
     self.surname_vocab = surname_vocab
     self.nationality_vocab = nationality_vocab
-
 
   def vectorize(self, surname: str) -> np.ndarray:
     """
@@ -119,6 +111,14 @@ class MLPVectorizer(object):
 
     return one_hot
 
+  @classmethod
+  def from_serializable(cls, contents: dict):
+    """
+      Instantiates a Vectorizer from a serializable dictionary
+    """
+    surname_vocab = Vocabulary.from_serializable(contents['surname_vocab'])
+    nationality_vocab = Vocabulary.from_serializable(contents['nationality_vocab'])
+    return cls(surname_vocab, nationality_vocab)
 
   @classmethod
   def from_dataframe(cls, surname_df: pd.DataFrame):
@@ -142,16 +142,6 @@ class MLPVectorizer(object):
 
     return cls(surname_vocab, nationality_vocab)
 
-  @classmethod
-  def from_serializable(cls, contents: dict):
-    """
-      Instantiates a Vectorizer from a serializable dictionary
-    """
-    surname_vocab = Vocabulary.from_serializable(contents['surname_vocab'])
-    nationality_vocab = Vocabulary.from_serializable(contents['nationality_vocab'])
-
-    return cls(surname_vocab=surname_vocab, nationality_vocab=nationality_vocab)
-
   def to_serializeable(self) -> dict:
     """
       Create the serializable dictionary for caching
@@ -160,5 +150,4 @@ class MLPVectorizer(object):
         'surname_vocab': self.surname_vocab.to_serializeable(),
         'nationality_vocab': self.nationality_vocab.to_serializeable()
         }
-
 
