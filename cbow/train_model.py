7ad332194c06db9d398187e021fa08a6577bded3
Sudarshan <su0@ornl.gov>
2019-03-27 17:26:21 -0400

    Added console trainer

---
+++ cbow/train_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env
+
+import logging
+import pandas as pd
+import torch
+
+from torch import nn
+from torch import optim
+from torch.utils.data import DataLoader
+
+from ignite.engine import Events, create_supervised_evaluator
+from ignite.metrics import Accuracy, Loss
+from ignite.contrib.handlers import ProgressBar
+
+from cbow.dataset import CBOWDataset, DataContainer
+from cbow.model import CBOWClassifier, ModelContainer
+from cbow.consts import consts
+from cbow.trainer import IgniteTrainer
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler()
+sh.setFormatter(logging.Formatter('%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(sh)
+
+
+if __name__=='__main__':
+  logger.info("Loading data...")
+  df = pd.read_csv(consts.proc_dataset_csv)
+  dc = DataContainer(df, consts.vectorizer_file, consts.batch_size, is_load=True)
+
+  logger.info("Instantiating model...")
+  classifier = CBOWClassifier(dc.vocabulary_size, consts.embedding_size)
+  loss_func = nn.CrossEntropyLoss()
+  optimizer = optim.Adam(classifier.parameters(), lr=consts.learning_rate)
+  scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, mode='min', factor=0.1,
+      patience=1)
+
+  mc = ModelContainer(classifier, optimizer, loss_func, scheduler)
+
+  pbar = ProgressBar(persist=True)
+  metrics = {'accuracy': Accuracy(), 'loss': Loss(loss_func)}
+
+  consts.num_epochs=2
+  logger.info(f"Running model for {consts.num_epochs} epochs on device {consts.device}")
+  ig = IgniteTrainer(mc, dc, consts, pbar, metrics)
+  ig.run()
